fbf2a3231c09db551bb4bfbc1e7e50cf38691532
Paulo Vicente <vcente82@gmail.com>
2022-08-08 18:34:19 -0400

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Header } from "./components/Header";
+import { TaskList } from "./components/TaskList";
 
 import "./styles/global.scss";
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div>
       <Header />
+      <TaskList />
     </div>
   );
 }
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,7 +1,75 @@
+import { PlusCircle } from "phosphor-react";
+import { useState } from "react";
+import styles from "./tasklist.module.scss";
+
+interface Task {
+  title: string;
+  id: number;
+  isComplete: boolean;
+}
+
 export const TaskList = () => {
+  const [tasks, setTasks] = useState<Task[]>([
+    {
+      id: 0,
+      title: "Beber agua",
+      isComplete: false,
+    },
+  ]);
+  const [newTask, setNewTaskList] = useState();
+
+  const handleToggleTaskComplete = (id: number) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === task.id ? { ...task, isComplete: !task.isComplete } : task
+      )
+    );
+  };
+
   return (
-    <div>
-      <div></div>
-    </div>
+    <section className={styles.taskContainer}>
+      <header className={styles.addTask}>
+        <input type="text" placeholder="Adicione uma nova tarefa" />
+        <button>
+          Criar
+          <PlusCircle size={22} />
+        </button>
+      </header>
+
+      <main className={styles.taskList}>
+        <div className={styles.info}>
+          <p>
+            Tarefas criadas <span>0</span>
+          </p>
+          <p>
+            Concluidas <span>0</span>
+          </p>
+        </div>
+
+        <section className={tasks.length ? styles.tasks : styles.noTasks}>
+          <ul>
+            {tasks.map((task) => (
+              <li key={task.id}>
+                <div
+                  className={
+                    task.isComplete ? styles.completed : styles.noCompleted
+                  }
+                >
+                  <label htmlFor="">
+                    <input
+                      type="checkbox"
+                      checked={task.isComplete}
+                      readOnly
+                      onClick={() => handleToggleTaskComplete(task.id)}
+                    />
+                  </label>
+                  <p>{task.title}</p>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </section>
+      </main>
+    </section>
   );
 };
